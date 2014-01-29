93ff37bd015719c20aa5badf5d370c45ed449498
OpenShift guest <kevanjohnson79@gmail.com>
2014-01-29 12:59:21 -0500

    Completed file

--- pc2.cpp
+++ pc2.cpp
@@ -44,7 +44,7 @@ bool isFactor (int numerator, int denominator)
 {
 	// CODE HERE
 	int remainder = numerator % denominator;
-	if (remainer == 0) 
+	if (remainder == 0) 
 	{
 	    return true;
 	}
@@ -67,6 +67,15 @@ bool isFactor (int numerator, int denominator)
 void makeChange (int initialValue, int& quarters, int& dimes, int& nickels, int& pennies)
 {
 	// CODE HERE
+	int change = initialValue;
+	quarters = change / 25;
+	change %= 25;
+	dimes = change / 10;
+	change %= 10;
+	nickels = change / 5;
+	change %= 5;
+	pennies = change / 1;
+	change %= 1;
 }
 
 /*
@@ -83,12 +92,18 @@ void makeChange (int initialValue, int& quarters, int& dimes, int& nickels, int&
 double launchHumanCannonball (double initialVelocity, double launchAngle)
 {
 	// CODE HERE
-	//
+	// 
 	// 1 convert launchAngle from degrees to radians [radangle = degangle * (PI/180)]
+    double radangle = launchAngle * (3.14159/180);
 	// 2 compute final horizontal/x velocity [xveloc = initialVelocity * cos(radangle)]
-	// 3 compute final vertical/y velocity [yveloc = initialVecity * sin(radangle) * -1]
+	double xveloc = initialVelocity * cos(radangle);
+	// 3 compute final vertical/y velocity [yveloc = initialVelocity * sin(radangle) * -1]
+	double yveloc = initialVelocity * sin(radangle) * -1;
 	// 4 compute time of flight [flighttime = (yveloc) * 2 / -9.8]
+	double flighttime = (yveloc) * 2 / -9.8;
 	// 5 compute horizontal/x distance traveled [xdistance = xveloc * flighttime]
+	double xdistance = xveloc * flighttime;
+    
 }
 
 /*
