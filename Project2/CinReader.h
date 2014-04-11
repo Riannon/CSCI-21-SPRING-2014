e83e1a3370d003bd97aa22b671858c0141f9e562
OpenShift guest <kevanjohnson79@gmail.com>
2014-04-11 17:29:14 -0400

    Project2 Files

--- Project2/CinReader.h
+++ Project2/CinReader.h
@@ -30,7 +30,7 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#pragma once
+//#pragma once
 
 #include <iostream>
 #include <string>
